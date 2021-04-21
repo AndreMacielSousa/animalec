d0bb6fd9ffc4ef06ed60d9c1dd84559ca8ad4c10
André Maciel Sousa <1300012@estudante.uab.pt>
2021-04-21 08:14:22 +0100

    First Commit

--- src/main.js
+++ src/main.js
@@ -3,10 +3,17 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+import VueSimpleAlert from "vue-simple-alert";
+import BootstrapVue from 'bootstrap-vue'
+import "bootstrap-vue/dist/bootstrap-vue.css"
+
+Vue.use(BootstrapVue)
+Vue.use(VueSimpleAlert)
+
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: (h) => h(App),
+  render: h => h(App)
 }).$mount("#app");
--- src/router/index.js
+++ src/router/index.js
@@ -1,30 +1,157 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "Home",
-    component: Home,
+    name: "home",
+    component: () => import("@/views/Home.vue")
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    path: "/login",
+    name: "login",
+    component: () => import("@/views/Login.vue")
   },
+  {
+    path: "/register",
+    name: "register",
+    component: () => import("@/views/Register.vue")
+  },
+  {
+    path: "/admin",
+    name: "admin",
+    component: () => import("@/views/Admin.vue")
+  },
+  {
+    path: "/admin/quizzes/add",
+    name: "addQuiz",
+    component: () => import("@/views/quizzes/AddQuiz.vue")
+  },
+  {
+    path: "/admin/quizzes/:quizId",
+    name: "editQuiz",
+    component: () => import("@/views/quizzes/EditQuiz.vue")
+  },
+  {
+    path: "/admin/questions",
+    name: "listQuestions",
+    component: () => import("@/views/questions/ListQuestions.vue")
+  },
+  {
+    path: "/admin/quizzes",
+    name: "listQuizzes",
+    component: () => import("@/views/quizzes/ListQuizzes.vue")
+  },
+  {
+    path: "/admin/questions/add",
+    name: "addQuestion",
+    component: () => import("@/views/questions/AddQuestion.vue")
+  },
+  {
+    path: "/admin/questions/:questionId",
+    name: "editQuestion",
+    component: () => import("@/views/questions/EditQuestion.vue")
+  },
+  {
+    path: "/admin/users",
+    name: "listUsers",
+    component: () => import("@/views/users/ListUsers.vue")
+  },
+  {
+    path: "/admin/users/add",
+    name: "addUser",
+    component: () => import("@/views/users/AddUser.vue")
+  },
+  {
+    path: "/admin/users/:userId",
+    name: "editUser",
+    component: () => import("@/views/users/EditUser.vue")
+  },
+  {
+    path: "/admin/animals",
+    name: "listAnimals",
+    component: () => import("@/views/animals/ListAnimals.vue")
+  },
+  {
+    path: "/admin/animals/:animalId",
+    name: "editAnimal",
+    component: () => import("@/views/animals/EditAnimal.vue")
+  },
+  {
+    path: "/admin/animals/add",
+    name: "addAnimal",
+    component: () => import("@/views/animals/AddAnimal.vue")
+  },
+  {
+    path: "/profile",
+    name: "profile",
+    component: () => import("@/views/Profile.vue"),
+    meta: {
+      rule: "user"
+    }
+  },
+  {
+    path: "/profile/edit",
+    name: "editProfile",
+    component: () => import("@/views/EditProfile.vue"),
+    meta: {
+      rule: "user"
+    }
+  },
+  {
+    path: "/ranking",
+    name: "ranking",
+    component: () => import("@/views/Ranking.vue"),
+    meta: {
+      rule: "user"
+    }
+  },
+  
+  {
+    path: "/quizzes",
+    name: "quizzes",
+    component: () => import("@/views/Quizzes.vue"),
+    meta: {
+      rule: "user"
+    }
+  },
+  {
+    path: "/quizzes/:quizId",
+    name: "quiz",
+    component: () => import("@/views/Quiz.vue"),
+    meta: {
+      rule: "user"
+    }
+  },
+  {
+    path: "/animals",
+    name: "animals",
+    component: () => import("@/views/Animals.vue"),
+    meta: {
+      rule: "user"
+    }
+  },
+  {
+    path: "/animals/:animalId",
+    name: "animal",
+    component: () => import("@/views/Animal.vue"),
+    meta: {
+      rule: "user"
+    }
+  },
+  // Redirect to 404 page, if no match found
+  {
+    path: "*",
+    component: () => import("@/views/Error404.vue")
+  }
 ];
 
 const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
-  routes,
+  routes
 });
 
 export default router;
